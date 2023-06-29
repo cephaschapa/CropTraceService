39e0d7c9dec4ce09ac61961b03996610497e8291
Milan Gruner <milangruner@gmail.com>
2023-06-29 18:17:40 +0200

    feat: add authentication to relevant product endpoints

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,6 +1,16 @@
 import { Product } from '../models/product.model.js';
+import { fetchUserByToken } from './auth.controller.js';
 
-export function create(req, res) {
+export async function create(req, res) {
+  let user;
+  try {
+    user = await fetchUserByToken(req);
+  } catch(err) {
+    console.log(err);
+    res.status(401).send({ message: 'User not authenticated! ' + err });
+    return;
+  }
+
   if (!req.body.name) {
     res.status(400).send({ message: 'name cannot be empty!' });
     return;
@@ -14,6 +24,7 @@ export function create(req, res) {
     destination: req.body.destination,
     isCertified: req.body.isCertified ? req.body.isCertified : false,
     storageSensorData: req.body.storageSensorData,
+    user: user._id,
   });
 
   product
@@ -62,10 +73,11 @@ export function findOne(req, res) {
 
   Product.findById(id)
     .then(data => {
-      if (!data)
+      if (!data) {
         res.status(404).send({ message: `Product with id ${id} not found!` });
-      else
+      } else {
         res.send(data);
+      }
     })
     .catch(err => {
       res.status(500).send({ message: `Failed to retrieve Product with id ${id}` });
@@ -73,7 +85,14 @@ export function findOne(req, res) {
     });
 }
 
-export function update(req, res) {
+export async function update(req, res) {
+  let user;
+  try {
+    user = await fetchUserByToken(req);
+  } catch(err) {
+    return res.status(401).send({ message: 'User not authenticated! ' + err });
+  }
+
   if (!req.body) {
     return res.status(400).send({
       message: 'Product data in request body cannot be empty!',
@@ -81,6 +100,17 @@ export function update(req, res) {
   }
 
   const id = req.params.id;
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(404).send({
+      message: `Failed to update product with id ${id}. Might not exist`,
+    });
+  } else if (!product.user.equals(user._id)) {
+    return res.status(401).send({
+      message: `You don't own this product, unable to edit!`,
+    });
+  }
+
   Product.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
     .then(data => {
       if (!data) {
@@ -99,10 +129,28 @@ export function update(req, res) {
     })
 }
 
-export function remove(req, res) {
+export async function remove(req, res) {
+  let user;
+  try {
+    user = await fetchUserByToken(req);
+  } catch(err) {
+    return res.status(401).send({ message: 'User not authenticated! ' + err });
+  }
+
   if (!req.params.id) {
     res.status(400).send({ message: 'id cannot be empty!' });
     return;
+  }
+
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(404).send({
+      message: `Failed to update product with id ${id}. Might not exist`,
+    });
+  } else if (!product.user.equals(user._id)) {
+    return res.status(401).send({
+      message: `You don't own this product, unable to delete!`,
+    });
   }
 
   const id = req.params.id;
