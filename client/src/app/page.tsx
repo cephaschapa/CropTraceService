064c4f95aed395408d74399c9e2e2a57d8facfd5
cephaschapa <cephaschapa@gmail.com>
2023-06-29 21:51:45 +0200

    feat: login authentication with next auth

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,14 +1,54 @@
 'use client'
 
+import React, {useState} from 'react'
+import {signIn, useSession} from "next-auth/react"
 import {MdEmail, MdPassword} from "react-icons/md";
 import {useRouter} from 'next/navigation';
+import Image from "next/image";
+
 
 export default function Home() {
   const isLoggedIn = true;
   const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const session = useSession()
+
+  console.log(session.data?.user)
+ 
+  const handleSubmit = (e:any) => {
+    e.preventDefault()
+    
+    let data = {
+      email,
+      password
+    }
+
+    signIn('credentials', {
+      ...data,
+      redirect: false
+    }).then(async(callback)=>{
+      if(callback?.ok){
+        console.log("Login was successfull")
+        router.push('/dashboard')
+      }
+
+      if(!callback?.ok){
+        console.log("Login was unsuccessfull")
+      }
+    })
+  }
 
   return (
     <main className='h-[100vh] w-[100vw] main flex items-center justify-end'>
+      <div className="border h-full w-full p-24">
+        <Image 
+          src="/images/logo.svg"
+          alt="Logo"
+          width={250}
+          height={250}
+        />
+      </div>
       <div className='h-full w-1/2 flex flex-col justify-center items-center bg-white shadow-lg p-5 py-10'>
         <div className="w-[500px]">
           <p
@@ -21,15 +61,16 @@ export default function Home() {
           <div
             className='flex input items-center gap-4 h-[44px] mb-6 border-[1px] border-neutral-[#D5DAE1] px-4 mt-4 rounded-[6px]'
           >
-            <input type='text' placeholder='Username' className='h-full w-full outline-none'/>
+            <input onChange={(e)=> setEmail(e.target.value)} type='text' placeholder='Username' className='h-full w-full outline-none'/>
           </div>
           <p className="text-[#333F51] font-[500]">Password</p>
           <div
             className='flex input items-center gap-4 h-[44px] mb-4 border-[1px] border-neutral-[#D5DAE1] px-4 mt-4 rounded-[6px]'
           >
-            <input type='password' placeholder='Password' className='h-full w-full outline-none'/>
+            <input onChange={(e)=> setPassword(e.target.value)} type='password' placeholder='Password' className='h-full w-full outline-none'/>
           </div>
           <button
+            onClick={handleSubmit}
             className='bg-[#16A34A] h-16 w-full rounded-md mt-4 text-white font-bold'
           >Log In</button>
           <div
